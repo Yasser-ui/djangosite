ac14a9a9775ba5797cde15622b6253ea3e7b41bb
Yasser-ui <yasser.benyoussef@isigk.u-kairouan.tn>
2021-12-21 23:38:52 +0100

    Integrate Bootstrap template

--- reserve/views.py
+++ reserve/views.py
@@ -3,7 +3,13 @@ from django.utils import timezone
 
 import reserve
 from .models import Reserve
+from django.utils import timezone
+#from .forms import ReserveForm
 
 def index(request):
-    reserves = 
+    reserves = Reserve.objects.filter(published_date=timezone.now()).order_by('published_date')
     return render(request, 'reserve/index.html', {'reserve': reserves})
+
+#def reserve_new(request):
+    #form = ReserveForm()
+    #return render (request, 'reserve/index.html', {'form': form})
